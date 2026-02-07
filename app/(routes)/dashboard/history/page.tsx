242488f2cadee879a5d8583fe0ff186658d422bc
Abdul Basith <your_github_email_here>
2026-02-08 01:07:26 +0530

    fix ssr error

--- app/(routes)/dashboard/history/page.tsx
+++ app/(routes)/dashboard/history/page.tsx
@@ -1,7 +1,14 @@
 'use client'
-
 import React from "react";
-import HistoryList from "../_components/HistoryList";
+import dynamic from 'next/dynamic';
+
+const HistoryList = dynamic(
+  () => import('../_components/HistoryList'),
+  { 
+    ssr: false,
+    loading: () => <div className="p-6">Loading history...</div>
+  }
+);
 
 function History() {
   return (
